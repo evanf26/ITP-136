202221face5103b7a2fe4a167a2431e0b3f32394
evanf26 <115671591+evanf26@users.noreply.github.com>
2025-09-30 14:42:44 -0400

    setup

---
+++ Module2/Module2Assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Module2Assignment
+{
+    static void Main()
+    {
+        // create string variable
+        string patientName;
+
+        //ask the user for the patient name
+        Console.WriteLine("who is being seen today");
+    }
+}
