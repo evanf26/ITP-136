2f21216bccafd0ddbe0888efdd07694b15d0ce74
evanf26 <115671591+evanf26@users.noreply.github.com>
2025-10-01 19:23:24 -0400

    Module 5 assignment

---
+++ Module5Assignment/Module5Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Module5Assignment
+{
+    using static System.Console;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //take start and end numberes
+            WriteLine("Enter a number to start counting from:");
+            int startNumber = int.Parse(Console.ReadLine());
+            WriteLine("Enter a number to stop counting on:");
+            int endNumber = int.Parse(Console.ReadLine());
+
+            WriteLine("Starting counting from " + startNumber + " to " + endNumber);
+            //re-using startNumber as the counter
+            while (startNumber <= endNumber)
+            {
+                WriteLine(startNumber);
+                startNumber++;
+            }
+        }
+    }
+}
