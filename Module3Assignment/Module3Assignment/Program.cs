82f9f504657140c0f18e79d2fbef2453e81af0d8
evanf26 <115671591+evanf26@users.noreply.github.com>
2025-10-01 18:48:23 -0400

    Module 3 Assignment Complete

---
+++ Module3Assignment/Module3Assignment/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Module3Assignment
+{
+    using static System.Console;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //initialize an input variable, as ReadLine returns a string
+            string input;
+
+            //simple greeting to the user
+            WriteLine("Hello, today we will be itemizing a repair shop bill.");
+
+            //ask the user for the price of each service, then save accordingly
+            WriteLine("Please enter the total price for the oil change:");
+            //parse the string to a float
+            input = ReadLine();
+            float oilChange = float.Parse(input);
+
+            //not relevant but I had a flat tire on Sunday and was stuck at work until 1 am
+            WriteLine("Please enter the total price for the tire change:");
+            input = ReadLine();
+            float tireChange = float.Parse(input);
+
+            WriteLine("Please enter the total price for the car inspection:");
+            input = ReadLine();
+            float inspection = float.Parse(input);
+
+            float total = (oilChange + tireChange + inspection);
+            //output the price of service to the user
+            WriteLine("Subtotal for services performed = $" + total
+                + "\n6% tax for services performed = $" + (total * 0.06)
+                + "\nTotal amount due = $" + (total * 1.06));
+        }
+    }
+}
