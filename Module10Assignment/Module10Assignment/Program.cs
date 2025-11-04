93ee43e7947c9c35225856c1d643a05b25da2b3c
evanf26 <115671591+evanf26@users.noreply.github.com>
2025-11-04 15:29:00 -0500

    Module 10 Assignment

---
+++ Module10Assignment/Module10Assignment/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Module10Assignment
+{
+    using static System.Console;
+
+    public class Food
+    {
+        //constructor
+        public Food(int id, string name, string description, double cost)
+        {
+            FoodID = id;
+            Name = name;
+            Description = description;
+            Cost = cost;
+        }
+        //food properties
+        public int FoodID {  get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Cost { get; set; }
+    }
+
+    class Program
+    {
+        //main
+        static void Main(string[] args)
+        {
+            //create food objects
+            Food carrot = new Food(1,"Carrot","Root vegetable",1.5);
+            Food potato = new Food(2, "Potato", "Tuberous vegerable", 2.25);
+
+            //print header line
+            WriteLine("ID".PadRight(5) + "Name".PadRight(15) + "Description".PadRight(36) + "Cost\n" + new string('=', 60));
+
+            //print food objects
+            WriteLine((carrot.FoodID).ToString("D4") + " " + (carrot.Name).PadRight(15) + carrot.Description + $"{carrot.Cost:C}".PadLeft(40 - carrot.Description.Length));
+            WriteLine((potato.FoodID).ToString("D4") + " " + (potato.Name).PadRight(15) + potato.Description + $"{potato.Cost:C}".PadLeft(40 - potato.Description.Length));
+        }
+    }
+}
